3b383884633910b9ba7ddeb9276169d0bbafc5e4
naveenmurthy <naveenneogi@gmail.com>
2016-05-08 20:44:43 -0700

    cleanup commented code

---
+++ main/ContactsDump.java
@@ -0,0 +1,9 @@
+package com.ejenta;
+
+import java.util.*;
+
+/**
+ * Created by naveenmurthy on 5/6/16.
+ */
+//public class ContactsDump extends Multimaps<Integer, Integer> {}
+public class ContactsDump extends HashMap<Integer, HashSet<Integer>> {}
