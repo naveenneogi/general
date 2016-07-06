35e4ac1fce9d44af458e82d01a5e46e8a25d75ef
naveenmurthy <naveenneogi@gmail.com>
2016-07-06 15:14:45 -0700

    initial commit of a map/reduce algo for range queries

---
+++ Interview/src/com/workday/MapReduceRangeContainer.java
@@ -0,0 +1,11 @@
+package com.workday;
+
+/**
+ * Created by naveenmurthy on 7/6/16.
+ */
+public class MapReduceRangeContainer implements RangeContainer {
+    @Override
+    public Ids findIdsInRange(long fromValue, long toValue, boolean fromInclusive, boolean toInclusive) {
+        return null;
+    }
+}
