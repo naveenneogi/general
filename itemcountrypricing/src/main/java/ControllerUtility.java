dca15578d08938224bce650ea4504d5762b52ea3
Naveen Murthy <nmurthy@netflix.com>
2021-07-05 22:51:13 -0700

    init checkin for itemcountrypricing

---
+++ itemcountrypricing/src/main/java/ControllerUtility.java
@@ -0,0 +1,11 @@
+public class ControllerUtility {
+
+    public static void main(String[] args) throws InterruptedException {
+        PreaggUtility preaggUtility = PreaggUtility.getInstance();
+        preaggUtility.init();
+
+
+
+    }
+
+}
